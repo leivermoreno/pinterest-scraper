bc39c843df542db61214cdf44296c08df1ae64ba
Leiver Moreno <leivermoreno@gmail.com>
2024-03-12 09:34:56 -0400

    add exclude_duplicates method to Url model

--- playwright-scraper/db.py
+++ playwright-scraper/db.py
@@ -1,6 +1,8 @@
+from typing import Iterable
+
 from settings import SQLITE_DB_PATH
-from sqlalchemy import Engine, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Engine, create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -13,6 +15,17 @@ class Url(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     scraped: Mapped[bool]
     url: Mapped[str]
+
+    @staticmethod
+    def exclude_duplicates(session: Session, urls: Iterable) -> list[str]:
+        deduplicated_urls = []
+        for url in urls:
+            stmt = select(Url).where(Url.url == url)
+            result = session.scalars(stmt).first()
+            if result is None:
+                deduplicated_urls.append(url)
+
+        return deduplicated_urls
 
 
 def setup_db() -> Engine:
