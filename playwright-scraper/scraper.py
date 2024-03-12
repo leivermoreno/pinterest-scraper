e8982e249ab202591fc03a38b38fa1b10115dc52
Leiver Moreno <leivermoreno@gmail.com>
2024-03-12 09:39:06 -0400

    add method to scrape board urls

--- playwright-scraper/scraper.py
+++ playwright-scraper/scraper.py
@@ -1,12 +1,14 @@
 import logging
 import urllib.parse
-from typing import Iterator
+from typing import Iterable, Iterator
 
 from browser import BrowserManager
+from db import Url as UrlModel
 from db import setup_db
 from settings import OUTPUT_DIR
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
+from views.board_grid import BoardGridView
 
 
 class Scraper:
@@ -33,9 +35,27 @@ class Scraper:
         )
         self.logger.info("Scraper setup complete")
 
+    def join_urls(self, urls: Iterable) -> list[str]:
+        return [urllib.parse.urljoin(self.base_url, url) for url in urls]
+
+    def scrape_board_urls(self) -> list[str]:
+        browser = self.browser_manager.get_browser(self.initial_url)
+        with browser as page:
+            self.logger.info(f"Scraping board urls from {self.initial_url}")
+            view = BoardGridView(page=page)
+            view.start_view()
+
+        urls = view.get_board_urls()
+        processed_urls = self.join_urls(urls)
+        processed_urls = UrlModel.exclude_duplicates(self.session, processed_urls)
+        self.logger.info(f"Found {len(processed_urls)} board urls")
+
+        return processed_urls
+
     def run(self) -> None:
         try:
             self.setup()
+            board_urls = self.scrape_board_urls()
         finally:
             if self.session is not None:
                 self.session.close()
