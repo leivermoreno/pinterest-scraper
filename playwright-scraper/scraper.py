5015eb1123713e229fb35b33b864dea2b154e330
Leiver Moreno <leivermoreno@gmail.com>
2024-03-13 15:07:57 -0400

    add pin urls scraping functionality

--- playwright-scraper/scraper.py
+++ playwright-scraper/scraper.py
@@ -9,13 +9,14 @@ from settings import OUTPUT_DIR
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 from views.board_grid import BoardGridView
+from views.pin_grid import PinGridView
 
 
 class Scraper:
     def __init__(self, query: str) -> None:
-        query = urllib.parse.quote_plus(query)
+        self.query = urllib.parse.quote_plus(query)
         self.base_url = "https://www.pinterest.com"
-        self.initial_url = f"{self.base_url}/search/boards/?q={query}&rs=typed"
+        self.initial_url = f"{self.base_url}/search/boards/?q={self.query}&rs=typed"
         self.output_dir = OUTPUT_DIR
         self.engine: Engine
         self.session: Session
@@ -54,10 +55,41 @@ class Scraper:
 
         return processed_urls
 
+    def scrape_board_pins(self, url: str) -> list[str]:
+        browser = self.browser_manager.get_browser(url)
+        with browser as page:
+            self.logger.info(f"Scraping board {url}")
+            view = PinGridView(page=page)
+            view.start_view()
+
+        urls = view.get_pin_urls()
+        processed_urls = self.join_urls(urls)
+        processed_urls = UrlModel.exclude_duplicates(self.session, processed_urls)
+        self.logger.info(f"Found {len(processed_urls)} pin urls for board {url}")
+
+        return processed_urls
+
+    def scrape_boards(self, urls: list[str]) -> None:
+        for url in urls:
+            pin_urls = self.scrape_board_pins(url)
+            self.session.add_all(
+                [
+                    UrlModel(
+                        pin_url=pin_url,
+                        board_url=url,
+                        query=self.query,
+                        scraped=False,
+                    )
+                    for pin_url in pin_urls
+                ]
+            )
+            self.session.commit()
+
     def run(self) -> None:
         try:
             self.setup()
             board_urls = self.scrape_board_urls()
+            self.scrape_boards(board_urls)
         finally:
             if self.session is not None:
                 self.session.close()
