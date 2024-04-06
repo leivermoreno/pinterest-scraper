7c0bc04c70676ebf97b553259df9c21c977456f1
Leiver Moreno <leivermoreno@gmail.com>
2024-04-06 13:18:12 -0400

    add creation of db if does not exist

--- scrapy_scraper/db.py
+++ scrapy_scraper/db.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
 
 from scrapy_scraper.settings import POSTGRES_HOST, POSTGRES_USER, POSTGRES_USER_PASSWORD
 
 engine = create_engine(
     f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_USER_PASSWORD}@{POSTGRES_HOST}/pinterest_scraper"
 )
+
+if not database_exists(engine.url):
+    create_database(engine.url)
 
 Base = automap_base()
 Base.prepare(autoload_with=engine)
